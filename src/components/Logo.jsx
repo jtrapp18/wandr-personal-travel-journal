0bc435a77a629a742fbc0320f71a4e5e48a29028
+ <jacqueline.trapp18@gmail.com>
2024-12-02 07:49:38 -0500

    updating logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -7,7 +7,7 @@ function Logo() {
           to="/"
           className="nav-link home"
         >
-          <img src="/images/wandr.png" alt="home"/>
+          <img src="/images/logos/wandr.png" alt="home"/>
         </NavLink>
       </div>
   );
--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -3,17 +3,11 @@ import { NavLink } from "react-router-dom";
 function NavLinks() {
   return (
     <div id="link-container">
-      {/* <NavLink
-        to="/"
-        className="nav-link home"
-      >
-        <img src="/images/passport.png" alt="passport"/>My Trips
-      </NavLink> */}
       <NavLink
         to="/new-trip"
         className="nav-link"
       >
-          <img src="/images/suitcase.png" alt="suitcase"/>Add to Bucketlist
+          <img src="/images/icons/suitcase.png" alt="suitcase"/>Add to Bucketlist
       </NavLink>
     </div>
   );
