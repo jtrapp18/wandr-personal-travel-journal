cf468fcef35b51182e3f86c13826b3774696b04e
+ <jacqueline.trapp18@gmail.com>
2024-12-01 20:58:42 -0500

    adding logos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ const Header = () => {
     return (
         <header>
             <div id="ext-header">
+                <img src="/images/logo_header.png" alt="logo" />
             </div>
             <NavBar />
         </header>
