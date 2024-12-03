bf1e36fa1cf4a7f3470dc51b72e912367333c2e4
+ <jacqueline.trapp18@gmail.com>
2024-12-03 13:39:48 -0500

    refactoring styling components

--- src/components/Attendees.jsx
+++ src/components/Attendees.jsx
@@ -1,13 +1,27 @@
+import styled from "styled-components";
 
+const NewAttendees = styled.div`
+    display: flex;
+    justify-content: end;
+
+    span {
+        padding: 0px 10px 0px 10px;
+        margin: 10px 0px 10px 10px;
+        background-color: var(--green);
+        border: 2px solid var(--dark-green);
+        border-radius: 5px;
+        font-size: 15px;
+    }
+`
 const Attendees = ({attendees}) => {
     return (
-        <div id="new-attendees">
+        <NewAttendees>
             {attendees.map(attendee=>
                 <span key={attendee}>
                     {attendee}
                 </span>
             )}
-        </div>
+        </NewAttendees>
     );
 }
 
--- src/components/TripCard.jsx
+++ src/components/TripCard.jsx
@@ -1,6 +1,66 @@
-import { Navigate, useNavigate} from "react-router-dom";
+import { useNavigate} from "react-router-dom";
 import { formatDate } from "../helper";
 import Attendees from "./Attendees";
+import styled from "styled-components";
+
+const StyledTripCard = styled.article`
+    height: 300px;
+    padding: 10px;
+    position: relative;
+
+    img {
+        width: 100%;
+        height: 85%;
+        object-fit: cover;
+        overflow: hidden;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    h2 {
+        position: absolute;
+        background-color: rgba(0,0,0,.5);
+        margin-left: 5px;
+        color: white;
+    }
+
+    p.globe {
+        bottom: 0px;
+        right: 0px;
+        font-size: 25px;
+        height: 15%;
+    }
+
+    .grayscale {
+        filter: grayscale(100%) brightness(1.2);
+    }
+
+    details p {
+      color: var(--dark-green);
+    }
+
+    &:hover h2 {
+      zoom: 130%;
+    }
+
+    details summary:hover {
+        font-weight: bold;
+    }
+
+    &:has(details[open]) {
+        padding-bottom: 75px;
+    }
+`
+
+const TripInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+TripInfo.displayName = 'trip-info'
+
+const GlobeContainer = styled.div`
+  justify-content: left;
+`
 
 const TripCard = ({id, image, location, description, startDate, endDate, attendees, complete, rating}) => {
     const navigate = useNavigate();
@@ -31,24 +91,24 @@ const TripCard = ({id, image, location, description, startDate, endDate, attende
     }
 
     return (
-        <article className="trip-card" >
+        <StyledTripCard>
             <h2>{location}</h2>
             <img src={image} alt={image} onClick={handleClick} />
-            <div className="trip-info-container">
+            <TripInfo>
                 <div>
                     <p>{`${formatDate(startDate)}-${formatDate(endDate)}`}</p>
                 </div>
-                <div className="globe-container">
+                <GlobeContainer>
                     <p className={complete ? "globe" : "globe grayscale"}>🌍</p>
-                </div>
-            </div>
+                </GlobeContainer>
+            </TripInfo>
             <details>
                 <summary>Details</summary>
                 <p>{description}</p>
                 <Attendees attendees={attendees}/>
                 {addlDetails}
             </details>
-        </article>
+        </StyledTripCard>
     );
 }
 
