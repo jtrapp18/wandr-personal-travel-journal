841da2e7a711187ecb27574921834da99e2bd2e1
+ <jacqueline.trapp18@gmail.com>
2024-12-03 22:22:43 -0500

    restructuring routing logic

--- src/routes.js
+++ src/routes.js
@@ -2,53 +2,34 @@
 import App from "./App";
 import Home from "./pages/Home";
 import NewTrip from "./pages/NewTrip";
-import Page2 from "./pages/Page2"
 import ErrorPage from "./pages/ErrorPage";
 import TripReview from "./pages/TripReview";
 import TripItinerary from "./pages/TripItinerary";
 
 const routes = [
   {
-      path: "/",
-      element: <App />,
-      errorElement: <ErrorPage />,
-      children: [
-           {
-              path: "/",
-              element: <Home />,
-              children: [
-                {
-                    path: "/review/:id",
-                    element: <TripReview />
-                },
-                {
-                  path: "/itinerary/:id",
-                  element: <TripItinerary />
-                }
-              ]
-          }, 
-          {
-              path: "/new-trip",
-              element: <NewTrip />
-              // children: [
-              //   {
-              //       path: "/review/:id",
-              //       element: <TripReview />
-              //   }
-              // ]
-          },
-          {
-              path: "/page-2",
-              element: <Page2 />
-              // children: [
-              //   {
-              //       path: "/itinerary/:id",
-              //       element: <TripItinerary />
-              //   }
-              // ]
-          }
-      ]
-  }
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "new-trip",
+        element: <NewTrip />,
+      },
+      {
+        path: "review/:id", // Top-level route for TripReview
+        element: <TripReview />,
+      },
+      {
+        path: "itinerary/:id", // Top-level route for TripItinerary
+        element: <TripItinerary />,
+      },
+    ],
+  },
 ];
 
 export default routes;
